f3dca5f613e6eac3124a2a3aca5dce38630973f4
Jack Rabe <jhrabe@wisc.edu>
2021-05-19 16:00:17 -0500

    renamed variables to clear up confusion

--- scrabble.js
+++ scrabble.js
@@ -4,24 +4,17 @@ const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 const points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10];
 
 function solve() {
-    let board = getBoard(); //create a 2D array matrix
-
+    let board = new Array(15);
+    for (let i = 0; i < 15; i++) {
+        board[i] = new Array(15);
+    }
     board = getLetters(board); //fill in the letters from the user
 
     let hand = document.getElementById("hand").value.toLowerCase();
 
-    getPossibleWords(hand);
+    let possibleWords = getPossibleWords(hand);
 }
 
-function getBoard() {
-    let board = new Array(15);
-
-    for (let i = 0; i < 15; i++) {
-        board[i] = new Array(15);
-    }
-
-    return board;
-}
 
 function getLetters(board) {
     let i = 0;
@@ -47,34 +40,27 @@ async function getPossibleWords(hand) {
     possible = new Array();
 
     createList("", hand, allWords);
-
     setTimeout(() => {
         calcPoints(possible);
     }, 2000);
 }
 
 function createList(start, otherLetters, allWords) {
-    let temp = start;
+    let startOfWord = start;
 
-    for (let i = 0; i < otherLetters.length; i++) {
-        let letter = otherLetters[i];
-
-        start = temp + letter;
-        let otherLetters2 = otherLetters.replace(letter, "");
+    for (const letter of otherLetters) {
+        start = startOfWord + letter;
+        let remaining = otherLetters.replace(letter, "");
         let check = checkString(start, allWords);
 
-        if (check === true) { //if this is a whole word
-            if (possible.indexOf(start) == -1) {
-                console.log(`possible: ${start}`);
+        if (check === true && possible.indexOf(start) == -1) { // this is a whole word contained in the list
                 possible.push(start);
-            }
-            if (otherLetters2.length != 0) {
-                createList(start, otherLetters2, allWords);
-            }
-        } else if (check === null) {
+}
+        else if (check === null) {  // no word exists with these starting characters
             return;
-        } else if (otherLetters2.length != 0) {
-            //console.log(start);
+        }
+        
+        if (remaining.length != 0) {  // not a complete word, but letters still remain
             createList(start, otherLetters2, allWords);
         }
     }
@@ -83,18 +69,13 @@ function createList(start, otherLetters, allWords) {
 function checkString(str, allWords) {
     let prev = allWords[str.charAt(0)];
 
-    //console.log(`checking: ${str}`);
-
     for (let i = 1; i < str.length; i++) {
         let char = str.charAt(i);
 
-        if (i == (str.length - 1)) { //last letter
-            if ((prev[char]) && (prev[char].end)) {
-                return true;
-            } else {
-                return false;
-            }
-        } else { //not the last letter in the string
+        if (i == (str.length - 1)) { // last letter
+            return (prev[char]) && (prev[char].end);
+        } 
+        else { // not the last letter in the string
             if (prev[char] === undefined) {
                 return null;
             } else {
@@ -110,7 +91,6 @@ function calcPoints(array) {
         let word = array[i];
         pointArray.push(calcWord(word));
 
-
         if (i == (array.length - 1)) {
             console.log(array);
             console.log(pointArray);
@@ -121,8 +101,8 @@ function calcPoints(array) {
             let sortedWords = sortWords(array, oldPointArray, pointArray);
 
 
-            for (let i = 0; i < sortedWords.length; i++) {
-                console.log(`${sortedWords[i]}: ${pointArray[i]}`);
+            for (let j = 0; j < sortedWords.length; j++) {
+                console.log(`${sortedWords[j]}: ${pointArray[j]}`);
             }
         }
     }
@@ -137,10 +117,16 @@ function calcWord(word) { //get point values of a word
             return totalPoints;
         }
     }
+    // let totalPoints = 0;
+    // for (const letter of word){
+    //     totalPoints += points[letters.indexOf(letter)];
+    // }
+    // return totalPoints;
 }
 
 function sortWords(words, oldPointArray, pointArray) {
     let sortedWords = new Array();
+    console.log("hi")
     for (let i = 0; i < words.length; i++) {
         let index = oldPointArray.indexOf(pointArray[i]);
 

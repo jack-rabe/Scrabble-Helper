ee2c8481a99c53ea219c539dd70c700eded3d0ac
Jack Rabe <jhrabe@wisc.edu>
2021-05-19 20:51:10 -0500

    fixed check word logic error

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Scrabble-Helper
+# Scrabble Helper
 
 Hopefully will eventually maybe help you with a word or two in Scrabble.
 
--- scrabble.js
+++ scrabble.js
@@ -3,7 +3,7 @@ var possible = new Array();
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10];
 
-function solve() {
+async function solve() {
     let board = new Array(15);
     for (let i = 0; i < 15; i++) {
         board[i] = new Array(15);
@@ -12,15 +12,16 @@ function solve() {
 
     let hand = document.getElementById("hand").value.toLowerCase();
     
-    getPossibleWords(hand);
+    await getPossibleWords(hand);
 
-    addTopWordsToTags();
+    addTopWordsToTags(possible);
 }
 
-function addTopWordsToTags(){
+function addTopWordsToTags(sortedWordArray){
     const bestWordTags = document.getElementsByClassName("best-words");
-    for (const tag of bestWordTags){
-        tag.innerHTML = "word here";  // TODO add logic to get the actual top three words here
+    for (let index = 0; index < 3; index++){
+        const tag = bestWordTags[index];
+        tag.innerHTML = sortedWordArray[index];  // TODO add logic to get the actual top three words here
     }
 }
 
@@ -48,20 +49,22 @@ async function getPossibleWords(hand) {
     possible = new Array();
 
     createList("", hand, allWords);
+    
     setTimeout(() => {
-        calcPoints(possible);
+        possible = calcPoints(possible);  // sort the words in descending point order
     }, 2000);
 }
 
 function createList(start, otherLetters, allWords) {
     let startOfWord = start;
 
-    for (const letter of otherLetters) {
+    for (let i = 0; i < otherLetters.length; i++) {
+        const letter = otherLetters[i];
         start = startOfWord + letter;
         let remaining = otherLetters.replace(letter, "");
         let check = checkString(start, allWords);
 
-        if (check === true && possible.indexOf(start) == -1) { // this is a whole word contained in the list
+        if (check && possible.indexOf(start) == -1) { // this is a whole word contained in the list
                 possible.push(start);
 }
         else if (check === null) {  // no word exists with these starting characters
@@ -74,16 +77,20 @@ function createList(start, otherLetters, allWords) {
     }
 }
 
+
 function checkString(str, allWords) {
     let prev = allWords[str.charAt(0)];
 
     for (let i = 1; i < str.length; i++) {
         let char = str.charAt(i);
 
-        if (i == (str.length - 1)) { // last letter
-            return (prev[char]) && (prev[char].end);
-        } 
-        else { // not the last letter in the string
+        if (i == (str.length - 1)) { //last letter
+            if ((prev[char]) && (prev[char].end)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else { //not the last letter in the string
             if (prev[char] === undefined) {
                 return null;
             } else {
@@ -95,52 +102,32 @@ function checkString(str, allWords) {
 
 function calcPoints(array) {
     let pointArray = new Array();
-    for (let i = 0; i < array.length; i++) {
-        let word = array[i];
+    for (const word of array) {
         pointArray.push(calcWord(word));
-
-        if (i == (array.length - 1)) {
-            console.log(array);
-            console.log(pointArray);
-            let oldPointArray = JSON.parse(JSON.stringify(pointArray)); //deep dupe array
-
-            pointArray = pointArray.sort((a, b) => b - a); //sort the array
-
-            let sortedWords = sortWords(array, oldPointArray, pointArray);
-
-
-            for (let j = 0; j < sortedWords.length; j++) {
-                console.log(`${sortedWords[j]}: ${pointArray[j]}`);
-            }
-        }
     }
+    console.log(array);
+    console.log(pointArray);
+    let oldPointArray = JSON.parse(JSON.stringify(pointArray)); //deep dupe array
+    pointArray = pointArray.sort((a, b) => b - a); //sort the array of numbers
+
+    let sortedWords = sortWords(array, oldPointArray, pointArray);  // sort the array of words
+    return sortedWords;
 }
 
 function calcWord(word) { //get point values of a word
     let totalPoints = 0;
     for (let i = 0; i < word.length; i++) {
         totalPoints += (points[(letters.indexOf(word.charAt(i)))]);
-
-        if (i == (word.length - 1)) {
-            return totalPoints;
-        }
     }
-    // let totalPoints = 0;
-    // for (const letter of word){
-    //     totalPoints += points[letters.indexOf(letter)];
-    // }
-    // return totalPoints;
+    return totalPoints;
 }
 
 function sortWords(words, oldPointArray, pointArray) {
     let sortedWords = new Array();
     for (let i = 0; i < words.length; i++) {
         let index = oldPointArray.indexOf(pointArray[i]);
-
         sortedWords.push(words[index]);
-
-        if (i == (words.length - 1)) {
-            return sortedWords;
-        }
     }
+
+    return sortedWords;
 }
